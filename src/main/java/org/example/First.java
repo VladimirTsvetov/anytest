8159fa9b6ebb821c20464cdf40baf6c1efe7bcf9
VladimirTsvetov <v.a.tsvetov@gmail.com>
2023-03-20 19:33:15 +0300

    добавлен класс

---
+++ src/main/java/org/example/First.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class First {
+    private int a;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
